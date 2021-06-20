c63a28c538b056b429663f3cd852db5ea0a0cd8a
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-21 00:14:10 +0530

    [sohail] Add: customize message using string funtions and regex

--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -19,6 +19,7 @@ namespace AlgorithmProblems
             Console.WriteLine("Press 6 check two strings are Anagram");
             Console.WriteLine("Press 7 to print prime numbers that are Anagram and Palindrome in 0-1000 range");
             Console.WriteLine("Press 8 to Find the number.");
+            Console.WriteLine("Press 9 to Customize message string using Regex.");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -58,6 +59,16 @@ namespace AlgorithmProblems
                     int number = Convert.ToInt32(args[0]);
                     FindNumber findNumber = new FindNumber();
                     findNumber.SeachNumber(number, 1, 100);
+                    break;
+                case 9:
+                    //Customize Message Demonstration using string function and RegEx
+                    string words = "Hello <<name>>,\nWe have your fullname as << full name >> in our system. \nYour contact number is 91 - xxxxxxxxxx. Please,let us know in case of any clarification \nThank you BridgeLabz 01 / 01 / 2016.";
+                    Console.WriteLine(words);
+                    Console.WriteLine();
+                    Console.WriteLine("Do you want to recorrect details? Press Y --> Yes \n Press N --> No  ");
+                    char inp = Convert.ToChar(Console.ReadLine());
+                    UserDetails userDetails = new UserDetails();
+                    userDetails.UserChoice(inp);
                     break;
                 default:
                     Console.WriteLine("invalid selection made!!");
---
+++ AlgorithmProblems/UserDetails.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AlgorithmProblems
+{
+    class UserDetails
+    {
+        //properties
+        private string firstName;
+        private string lastName;
+        private long phoneNumber;
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public long PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+            set
+            {
+                phoneNumber = value;
+            }
+        }
+        /// <summary>
+        /// Display choice method
+        /// </summary>
+        /// <param name="inp"></param>
+        public void UserChoice(char inp)
+        {
+            inp = Uppercase(inp);
+            switch (inp)
+            {
+                case 'Y':
+                    //ask UserDetails//
+                    Details();
+                    break;
+                case 'N':
+                    Console.WriteLine("Exiting....");
+                    break;
+                default:
+                    Console.WriteLine("invalid input, try again!!");
+                    inp = Convert.ToChar(Console.ReadLine());
+                    UserChoice(inp);
+                    break;
+            }
+        }
+        /// <summary>
+        /// convert to Upper case
+        /// </summary>
+        /// <param name="ch"></param>
+        /// <returns></returns>
+        internal char Uppercase(char ch)
+        {
+            char input = Char.ToUpper(ch);
+            return input;
+        }
+        /// <summary>
+        /// Alter user details
+        /// </summary>
+        internal void Details()
+        {
+            Console.WriteLine("Enter FirstName: ");
+            FirstName = Console.ReadLine();
+            if (ValidateName(FirstName))
+            {
+                Console.WriteLine("First name saved successfully!!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid name format");
+            }
+            Console.WriteLine("Enter lastName: ");
+            LastName = Console.ReadLine();
+            if (ValidateName(LastName))
+            {
+                Console.WriteLine("Last name saved successfully!!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid name format");
+            }
+            Console.WriteLine("Enter phone Number");
+            string strPhoneNumber = Console.ReadLine();
+            if (ValidatePhoneNumber(strPhoneNumber))
+            {
+                PhoneNumber = Convert.ToInt64(strPhoneNumber);
+                Console.WriteLine("Phone Number saved successfully!!");
+            }
+            else
+            {
+                Console.WriteLine("invalid Number or format");
+            }
+            string words = $"Hello {FirstName},\nWe have your fullname as {FirstName + " " + LastName} in our system. \nYour contact number is {PhoneNumber}. Please,let us know in case of any clarification \nThank you BridgeLabz {DateTime.Today}.";
+            Console.WriteLine(words);
+        }
+        /// <summary>
+        /// validate Name using Regex
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        internal bool ValidateName(string name)
+        {
+            string nameRegex = "^[A-Z][a-z]{3,20}$";
+            if (Regex.IsMatch(name, nameRegex))
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Validate phone number using regex
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <returns></returns>
+        internal bool ValidatePhoneNumber(string phoneNumber)
+        {
+            string phonenumRegex = "^[6-9][0-9]{9,12}$";
+            if (Regex.IsMatch(phoneNumber, phonenumRegex))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
